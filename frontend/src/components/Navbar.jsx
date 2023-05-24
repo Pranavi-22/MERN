64f7ec943a5f546fb0ee76ab26de9dd8f0b2ce61
Pranavi22 <pranavialle@gmail.com>
2023-05-23 20:43:01 -0500

    firstcommit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+const Navbar=()=>{
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Mern</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link to="/" className="nav-link active" aria-current="page">Create Post</Link>
+        <Link to="/all" className="nav-link" href="#">All Post</Link>
+      </div>
+    </div>
+  </div>
+</nav>
+    );
+}
+
+export default Navbar;
